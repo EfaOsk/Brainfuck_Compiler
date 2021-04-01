a2fa1e9748cde43f948445d7899b178f29c606ae
EfaOsk <42963551+EfaOsk@users.noreply.github.com>
2021-04-01 16:31:29 +0000

    Changed Language
    Not a class any more, and a bit more hardcoded

--- language.h
+++ language.h
@@ -3,28 +3,37 @@
 
 #include <map>
 
-class Language {
-public:
-    enum Token {
-        t_shift_left = 256, // <
-        t_shift_right,      // >
-        t_plus,             // +
-        t_minus,            // -
-        t_output,           // .
-        t_input,            // ,
-        t_while,            // [
-        t_end_while,        // ]
-        t_eoi,              // End of input.
-        t_unknown           // unknown token.
-    };
-    static const std::map<char,Token> SymbolToToken;
-    static std::map<Token,char> TokenToText;
-    static void init() {
-        populate_token_to_text();
-    }
-
-private:
-    static void populate_token_to_text();
+enum Token {
+    t_shift_left = 256, // <
+    t_shift_right,      // >
+    t_plus,             // +
+    t_minus,            // -
+    t_output,           // .
+    t_input,            // ,
+    t_while,            // [
+    t_end_while,        // ]
+    t_eoi,              // End of input.
+    t_unknown           // unknown token.
+};
+const std::map<char, Token> SymbolToToken = {
+    {'<', Token::t_shift_left},
+    {'>', Token::t_shift_right},
+    {'+', Token::t_plus},
+    {'-', Token::t_minus},
+    {'.', Token::t_output},
+    {',', Token::t_input},
+    {'[', Token::t_while},
+    {']', Token::t_end_while},
+};
+const std::map<Token, char> TokenToChar = {
+    {Token::t_shift_left, '<'},
+    {Token::t_shift_right, '>'},
+    {Token::t_plus, '+'},
+    {Token::t_minus, '-'},
+    {Token::t_output, '.'},
+    {Token::t_input, ','},
+    {Token::t_while, '['},
+    {Token::t_end_while, '\0'},
 };
 
 #endif
