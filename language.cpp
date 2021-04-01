3dc562c9894e0ae321b1e66c5456060cbfac2a60
EfaOsk <42963551+EfaOsk@users.noreply.github.com>
2021-04-01 12:44:11 +0000

    Fix language

--- language.cpp
+++ language.cpp
@@ -4,24 +4,24 @@
 
 using namespace std;
 
-const map<char, Language::TokenName> Language::SymbolToTokenName = {
-    {'<', TokenName::t_shift_left},
-    {'>', TokenName::t_shift_right},
-    {'+', TokenName::t_plus},
-    {'-', TokenName::t_minus},
-    {'.', TokenName::t_output},
-    {',', TokenName::t_input},
-    {'[', TokenName::t_while},
-    {']', TokenName::t_end_while},
+const map<char, Language::Token> Language::SymbolToToken = {
+    {'<', Token::t_shift_left},
+    {'>', Token::t_shift_right},
+    {'+', Token::t_plus},
+    {'-', Token::t_minus},
+    {'.', Token::t_output},
+    {',', Token::t_input},
+    {'[', Token::t_while},
+    {']', Token::t_end_while},
 };
 
-map<Language::TokenName, char> Language::TokenNameToText;
+map<Language::Token, char> Language::TokenToText;
 
-void Language::populate_tokenname_to_text()
+void Language::populate_token_to_text()
 {
-    for ( auto& elem : Language::SymbolToTokenName ) {
-        TokenNameToText[elem.second] = elem.first;
+    for ( auto& elem : Language::SymbolToToken ) {
+        TokenToText[elem.second] = elem.first;
     }
-    TokenNameToText[TokenName::t_eoi] = "<eoi>";
-    TokenNameToText[TokenName::t_unknown] = "<unknown>";
+    TokenToText[Token::t_eoi] = '\0';
+    TokenToText[Token::t_unknown] = '?';
 }
--- language.h
+++ language.h
@@ -1,12 +1,11 @@
 #ifndef LANUGAGE_H
 #define LANUGAGE_H
 
-#include <string>
 #include <map>
 
 class Language {
 public:
-    enum TokenName {
+    enum Token {
         t_shift_left = 256, // <
         t_shift_right,      // >
         t_plus,             // +
@@ -18,11 +17,14 @@ public:
         t_eoi,              // End of input.
         t_unknown           // unknown token.
     };
-    static const std::map<std::char,TokenName> SymbolToTokenName;
-    static std::map<TokenName,std::char> TokenNameToText;
+    static const std::map<char,Token> SymbolToToken;
+    static std::map<Token,char> TokenToText;
+    static void init() {
+        populate_token_to_text();
+    }
 
 private:
-    static void populate_tokenname_to_text();
+    static void populate_token_to_text();
 };
 
 #endif
