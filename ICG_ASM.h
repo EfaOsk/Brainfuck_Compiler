5e0fbc2c37755dc04b10f0ac390c664898af2f24
EfaOsk <42963551+EfaOsk@users.noreply.github.com>
2021-04-09 12:44:06 +0000

    Ehv að reyna við ASM

--- ICG_ASM.h
+++ ICG_ASM.h
@@ -2,12 +2,18 @@
 #define ICG_ASM_H
 #include <map>
 #include "lexer.h"
+#include <stack>
 
 class ICG_ASM{
     public:
         ICG_ASM(Lexer* lex, std::ofstream& s): stream(s) {
             lexer = lex;
-            stream<<"add header";
+            label_co=0;
+            stream<<"global _start\n";
+            stream<<"section .text\n";
+            stream<<"out_put:\n  mov %rax, 1\n  mov %rdi, 1\n  mov %rsi, r8\n  mov %rdx, 1\n  syscall\n  ret\n";
+            stream<<"in_put:\n  mov %rax, 0\n  mov %rdi, 0\n  mov %rsi, r8\n  mov %rdx, 1\n  syscall\n  ret\n";
+            stream<<"_start:\n  mov r8, stack"<<std::endl;
             Token token;
             lexer->get(token);
             while (token!= Token::t_eoi)
@@ -41,37 +47,42 @@ class ICG_ASM{
                 }
                 lexer->get(token);
             }
-            stream<<"add ender";
+            stream<<"  mov eax,1  \n  int 0x80\n";
+            stream<<"section .bss\n";
         };
     private:
         Lexer* lexer;
         std::ofstream& stream;
+        std::stack<int> labels;
+        int label_co;
         void handel_plus(){
-            stream<<"add byte ptr [%%r8], 1"<<std::endl;
+            stream<<"  add $1, (r8)"<<std::endl;
         };
         void handel_minus(){
-            stream<<"sub byte ptr [%%r8], 1"<<std::endl;
+            stream<<"  sub $1, (r8)"<<std::endl;
         };
         void handel_shift_left(){
-            stream<<"sub r8, 1"<<std::endl;
+            stream<<"  dec r8"<<std::endl;
         };
         void handel_shift_right(){
-            stream<<"add r8, 1"<<std::endl;
+            stream<<"  inc r8"<<std::endl;
         };
         void handel_output(){
-            stream<<"output"<<std::endl;
+            stream<<"  call out_put"<<std::endl;
         };
         void handel_input(){
-            stream<<"input"<<std::endl;
+            stream<<"  call in_put"<<std::endl;
         };
         void handel_while(){
-            stream<<"start while loop"<<std::endl;
+            labels.push(++label_co);
+            stream<<"L"<<label_co<<":"<<std::endl;
         };
         void handel_end_while(){
-            stream<<"end while loop"<<std::endl;
+            stream<<"  loop L"<<labels.top()<<std::endl;
+            labels.pop();
         };
         void handel_eoi(){
-            stream<<"end of file"<<std::endl;
+            stream<<"  end of file"<<std::endl;
         };
 
 };
